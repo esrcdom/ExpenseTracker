6c81e4408c215b6ad9a0776251b9de5e5c32f4fa
esrcdom <marcedominguez95@outlook.com>
2022-09-26 18:31:24 -0300

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Marcelo Dominguez on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
